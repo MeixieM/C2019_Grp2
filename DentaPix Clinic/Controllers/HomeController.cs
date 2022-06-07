815b7af074805c800dec31e15648110fbe437af1
Alex Fernan <mercadoalexfernan@gmail.com>
2022-06-07 22:44:42 +0800

    Add project files.

---
+++ DentaPix Clinic/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DentaPix_Clinic.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DentaPix_Clinic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DentaPix Clinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DentaPix_Clinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
