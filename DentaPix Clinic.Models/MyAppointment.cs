b0dcacd19c62c2c54396d17fd00efb918b22bcce
Alex Fernan <mercadoalexfernan@gmail.com>
2022-06-22 04:16:45 +0800

    style(CustomerHomeView): add  video landing page & team section

---
+++ DentaPix Clinic.Models/MyAppointment.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DentaPix_Clinic.Models
+{
+    public class MyAppointment
+    {
+        public Appointment Appointment { get; set; }
+        [Range(1, 1, ErrorMessage = "Sorry But you can only book one appointment at a time")]
+        public int Count { get; set; }
+    }
+}
--- DentaPix Clinic/Areas/Customer/Controllers/HomeController.cs
+++ DentaPix Clinic/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using DentaPix_Clinic.Models;
+﻿using DentaPix_Clinic.DataAccess.Repository.IRepository;
+using DentaPix_Clinic.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,16 +8,36 @@ namespace DentaPix_Clinic.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            //this retrieves all patients from db
+            //IEnumerable<Patient> patientList = _unitOfWork.Patient.GetAll(includeProperties: "Doctor");
+
+            //this retrieves all doctors from db
+            IEnumerable<Doctor> doctorList = _unitOfWork.Doctor.GetAll();
+            return View(doctorList);
         }
+
+        //    public IActionResult Details(int id)
+        //    {
+        //        MyAppointment appObj = new()
+        //        {
+        //            Count = 1,
+        //            Appointment = _unitOfWork.Appointment.GetFirstOrDefault(u => u.AppointmendId == id);
+
+        //    }
+        //        return View(appObj);
+        //}
+
+
 
         public IActionResult Privacy()
         {
