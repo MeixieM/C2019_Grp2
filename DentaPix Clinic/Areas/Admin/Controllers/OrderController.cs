12122df40869d5ef68e3ed51a540c00fa0d6878b
Alex Fernan <mercadoalexfernan@gmail.com>
2022-07-04 02:01:10 +0800

    Payment Management Commit

--- DentaPix Clinic/Areas/Admin/Controllers/OrderController.cs
+++ DentaPix Clinic/Areas/Admin/Controllers/OrderController.cs
@@ -4,6 +4,8 @@ using DentaPix_Clinic.Models.ViewModels;
 using DentaPix_Clinic.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace DentaPix_Clinic.Areas.Admin.Controllers
@@ -35,73 +37,73 @@ namespace DentaPix_Clinic.Areas.Admin.Controllers
             return View(OrderVM);
         }
 
-        //[ActionName("Details")]
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Details_PAY_NOW()
-        //{
-        //    OrderVM.OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, includeProperties: "ApplicationUser");
-        //    OrderVM.OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == OrderVM.OrderHeader.Id, includeProperties: "Product");
+        [ActionName("Details")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details_PAY_NOW()
+        {
+            OrderVM.OrderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, includeProperties: "ApplicationUser");
+            OrderVM.OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderId == OrderVM.OrderHeader.Id, includeProperties: "Treatment");
 
-        //    //stripe settings 
-        //    var domain = "https://localhost:44300/";
-        //    var options = new SessionCreateOptions
-        //    {
-        //        PaymentMethodTypes = new List<string>
-        //        {
-        //          "card",
-        //        },
-        //        LineItems = new List<SessionLineItemOptions>(),
-        //        Mode = "payment",
-        //        SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderid={OrderVM.OrderHeader.Id}",
-        //        CancelUrl = domain + $"admin/order/details?orderId={OrderVM.OrderHeader.Id}",
-        //    };
+            //stripe settings 
+            var domain = "https://localhost:44331/";
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string>
+                {
+                  "card",
+                },
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+                SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderid={OrderVM.OrderHeader.Id}",
+                CancelUrl = domain + $"admin/order/details?orderId={OrderVM.OrderHeader.Id}",
+            };
 
-        //    foreach (var item in OrderVM.OrderDetail)
-        //    {
+            foreach (var item in OrderVM.OrderDetail)
+            {
 
-        //        var sessionLineItem = new SessionLineItemOptions
-        //        {
-        //            PriceData = new SessionLineItemPriceDataOptions
-        //            {
-        //                UnitAmount = (long)(item.Price * 100),//20.00 -> 2000
-        //                Currency = "usd",
-        //                ProductData = new SessionLineItemPriceDataProductDataOptions
-        //                {
-        //                    Name = item.Product.Title
-        //                },
+                var sessionLineItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)(item.Price * 100),//20.00 -> 2000
+                        Currency = "usd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Treatment.Name
+                        },
 
-        //            },
-        //            Quantity = item.Count,
-        //        };
-        //        options.LineItems.Add(sessionLineItem);
+                    },
+                    Quantity = item.Count,
+                };
+                options.LineItems.Add(sessionLineItem);
 
-        //    }
+            }
 
-        //    var service = new SessionService();
-        //    Session session = service.Create(options);
-        //    _unitOfWork.OrderHeader.UpdateStripePaymentID(OrderVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
-        //    _unitOfWork.Save();
-        //    Response.Headers.Add("Location", session.Url);
-        //    return new StatusCodeResult(303);
-        //}
+            var service = new SessionService();
+            Session session = service.Create(options);
+            _unitOfWork.OrderHeader.UpdateStripePaymentID(OrderVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+            _unitOfWork.Save();
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
 
-        //public IActionResult PaymentConfirmation(int orderHeaderid)
-        //{
-        //    OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderHeaderid);
-        //    if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
-        //    {
-        //        var service = new SessionService();
-        //        Session session = service.Get(orderHeader.SessionId);
-        //        //check the stripe status
-        //        if (session.PaymentStatus.ToLower() == "paid")
-        //        {
-        //            _unitOfWork.OrderHeader.UpdateStatus(orderHeaderid, orderHeader.OrderStatus, SD.PaymentStatusApproved);
-        //            _unitOfWork.Save();
-        //        }
-        //    }
-        //    return View(orderHeaderid);
-        //}
+        public IActionResult PaymentConfirmation(int orderHeaderid)
+        {
+            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == orderHeaderid);
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                var service = new SessionService();
+                Session session = service.Get(orderHeader.SessionId);
+                //check the stripe status
+                if (session.PaymentStatus.ToLower() == "paid")
+                {
+                    _unitOfWork.OrderHeader.UpdateStatus(orderHeaderid, orderHeader.OrderStatus, SD.PaymentStatusApproved);
+                    _unitOfWork.Save();
+                }
+            }
+            return View(orderHeaderid);
+        }
 
         [HttpPost]
         [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
@@ -128,65 +130,65 @@ namespace DentaPix_Clinic.Areas.Admin.Controllers
             return RedirectToAction("Details", "Order", new { orderId = orderHeaderFromDb.Id });
         }
 
-        //[HttpPost]
-        //[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult StartProcessing()
-        //{
-        //    _unitOfWork.OrderHeader.UpdateStatus(OrderVM.OrderHeader.Id, SD.StatusInProcess);
-        //    _unitOfWork.Save();
-        //    TempData["Success"] = "Order Status Updated Successfully.";
-        //    return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
-        //}
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        [ValidateAntiForgeryToken]
+        public IActionResult StartProcessing()
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(OrderVM.OrderHeader.Id, SD.StatusInProcess);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Status Updated Successfully.";
+            return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
+        }
 
-        //[HttpPost]
-        //[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult ShipOrder()
-        //{
-        //    var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, tracked: false);
-        //    orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
-        //    orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
-        //    orderHeader.OrderStatus = SD.StatusShipped;
-        //    orderHeader.ShippingDate = DateTime.Now;
-        //    if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
-        //    {
-        //        orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
-        //    }
-        //    _unitOfWork.OrderHeader.Update(orderHeader);
-        //    _unitOfWork.Save();
-        //    TempData["Success"] = "Order Shipped Successfully.";
-        //    return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
-        //}
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        [ValidateAntiForgeryToken]
+        public IActionResult ShipOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, tracked: false);
+            orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+            orderHeader.OrderStatus = SD.StatusShipped;
+            orderHeader.ShippingDate = DateTime.Now;
+            if (orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            {
+                orderHeader.PaymentDueDate = DateTime.Now.AddDays(30);
+            }
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["Success"] = "Order Shipped Successfully.";
+            return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
+        }
 
-        //[HttpPost]
-        //[Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult CancelOrder()
-        //{
-        //    var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, tracked: false);
-        //    if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
-        //    {
-        //        var options = new RefundCreateOptions
-        //        {
-        //            Reason = RefundReasons.RequestedByCustomer,
-        //            PaymentIntent = orderHeader.PaymentIntentId
-        //        };
+        [HttpPost]
+        [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+        [ValidateAntiForgeryToken]
+        public IActionResult CancelOrder()
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(u => u.Id == OrderVM.OrderHeader.Id, tracked: false);
+            if (orderHeader.PaymentStatus == SD.PaymentStatusApproved)
+            {
+                var options = new RefundCreateOptions
+                {
+                    Reason = RefundReasons.RequestedByCustomer,
+                    PaymentIntent = orderHeader.PaymentIntentId
+                };
 
-        //        var service = new RefundService();
-        //        Refund refund = service.Create(options);
+                var service = new RefundService();
+                Refund refund = service.Create(options);
 
-        //        _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
-        //    }
-        //    else
-        //    {
-        //        _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
-        //    }
-        //    _unitOfWork.Save();
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusRefunded);
+            }
+            else
+            {
+                _unitOfWork.OrderHeader.UpdateStatus(orderHeader.Id, SD.StatusCancelled, SD.StatusCancelled);
+            }
+            _unitOfWork.Save();
 
-        //    TempData["Success"] = "Order Cancelled Successfully.";
-        //    return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
-        //}
+            TempData["Success"] = "Order Cancelled Successfully.";
+            return RedirectToAction("Details", "Order", new { orderId = OrderVM.OrderHeader.Id });
+        }
 
         #region API CALLS
         [HttpGet]
